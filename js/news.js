8302d3f2ce21345bc56b4f8aff30da7a71acedfa
alohajt <alohajiatao@gmail.com>
2020-07-05 15:54:51 -0600

    news page done

--- js/news.js
+++ js/news.js
@@ -1,36 +1,31 @@
-const overallUpdates = document.querySelector(".updates")
+const overallUpdates = document.querySelector(".comment_container")
 const titleForm = document.querySelector("#title_form")
-// one form has two inputs
 const updateForm = document.querySelector("#update_form")
 // im also uploading an image
 
 
 updateForm.addEventListener("submit", displayUpdate)
 
-titleForm.addEventListener("submit", displayTitle)
+// titleForm.addEventListener("submit", displayTitle)
 
-function displayTitle(event) {
-    event.preventDefault()
-
-    const titleFormData = new FormData(event.target)
-    const titleText = titleFormData.get("title")
-
-    const title = document.createElement("h4")
-    title.textContent = titleText
-    overallUpdates.append(title)
-
-    event.target.reset()
-}
 
 function displayUpdate(event) {
     event.preventDefault()
 
     const updateFormData = new FormData(event.target)
     const updateText = updateFormData.get("update")
+    const titleText = updateFormData.get("title")
+    const imgUpload = updateFormData.get("image")
 
+    const title = document.createElement("h4")
+    title.textContent = titleText
     const update = document.createElement("p")
     update.textContent = updateText
-    updates.append(update)
+    const image = document.createElement("image")
+    image.src = imgUpload
+    
+
+    overallUpdates.append(title, update, image)
 
     event.target.reset()
 }
