6669d3d0726177366b54502e854b116c5ce2393a
alohajt <alohajiatao@gmail.com>
2020-07-07 14:49:57 -0600

    copy paste

--- js/register.js
+++ js/register.js
@@ -1,28 +1,38 @@
 const loginForm = document.querySelector(".login");
-console.log(loginForm)
-// loginForm = null ??? 
+console.log(loginForm);
+// loginForm = null ???
 loginForm.addEventListener("submit", handleLogin);
 
 function handleLogin(event) {
-    event.preventDefault();
-    const loginFormData = new FormData(event.target);
-    const username = loginFormData.get("username")
-    const password = loginFormData.get("password")
-    console.log(username)
-    console.log(password)
+  event.preventDefault();
+  const loginFormData = new FormData(event.target);
+  const username = loginFormData.get("username");
+  const password = loginFormData.get("password");
+  console.log(username);
+  console.log(password);
 
-    const loginBody =  { username:username, password:password } 
+  const loginBody = { username: username, password: password };
 
-    fetch("http://localhost:3000/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(loginBody)
-    }).then(respnse => respnse.json())
-        .then(result => {
-            console.log(result.token)
-            localStorage.setItem("token", result.token)
-        })
-    event.target.reset();
+  fetch("http://localhost:3000/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(loginBody),
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        console.log(response);
+        throw new Error("Login fails");
+      } else {
+        return response.json();
+      }
+    })
+    .then((result) => {
+      console.log(result.token);
+        localStorage.setItem("token", result.token);
+        window.location.href = "/"
+    })
+    .catch((error) => console.error(error));
+  event.target.reset();
 }
